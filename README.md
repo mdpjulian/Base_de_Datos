553b863a7df5caefdb8b5fa330f1d20cd14cdcfa
kunkkas <mdpjulian@gmail.com>
2021-06-05 23:25:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Base_de_Datos
+Repositorio con el material de clase de la materia
---
+++ tp1_persitencia_en_archivos/python/picle.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# encoding: utf8
+
+import pickle
+
+
+def guardar_puntajes(nombre_archivo, puntajes):
+    """ Guarda la lista de puntajes en el archivo.
+    Pre: nombre_archivo corresponde a un archivo válido,
+         puntajes corresponde a los valores a guardar
+    Post: se guardaron los valores en el archivo en formato pickle.
+    """
+
+    archivo = open(nombre_archivo, "wb")
+    pickle.dump(puntajes, archivo)
+    archivo.close()
+
+
+def recuperar_puntajes(nombre_archivo):
+    """ Recupera los puntajes a partir del archivo provisto.
+        Devuelve una lista con los valores de los puntajes.
+    Pre: el archivo contiene los puntajes en formato pickle
+    Post: la lista devuelta contiene los puntajes en el
+          mismo formato que se los almacenó.
+    """
+
+    archivo = open(nombre_archivo, "rb")
+    puntajes = pickle.load(archivo)
+    archivo.close()
+    return puntajes
+
+
+valores = [("Pepe", 108, "4:16"), ("Juana", 2315, "8:42")]
+guardar_puntajes("puntajes.dat", valores)
+puntajes = recuperar_puntajes("puntajes.dat")
+print(puntajes)
